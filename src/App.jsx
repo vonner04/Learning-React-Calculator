5d35dee1600b4aab260184ca14b03c1c409d1723
unknown <randyleean092m@gmail.com>
2024-01-26 13:00:37 +1300

    feat: new logic for choosing operations and digits.
    handled edge cases where user would add inf trailing zeroes, user starts with a decimal point, user adds multiple decimal points AND if the user chooses an operand but there are no digits.

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,54 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      //Prevent trailing zeroes.
+      if (state.currentOperand === "0" && payload.digit === "0") return state;
+
+      //If decimal start then format as 0.
+      if (payload.digit === "." && state.currentOperand == null) {
+        return { ...state, currentOperand: "0." };
+      }
+
+      //Do nothing if there is already a decimal point
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
+      //Add digit to calculator
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+
+    case ACTIONS.CHOOSE_OPERATION:
+      //Do nothing if there is no current operand
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      //Update operation if there is no previous operand
+      if (state.previousOperand == null && state.currentOperand != null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+
+      //Default functionality when an operation is already chosen
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
   }
 }
+
+function evaluate({ currentOperand, previousOperand, operation }) {}
 
 export default function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -35,8 +77,15 @@ export default function App() {
         </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch}></OperationButton>
       <DigitButton digit="1" dispatch={dispatch}></DigitButton>
       <DigitButton digit="2" dispatch={dispatch}></DigitButton>
